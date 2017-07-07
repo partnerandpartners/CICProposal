a649412d845427284a77db71badaa609190a2662
Greg Mihalko <greg@partnerandpartners.com>
2017-07-07 12:49:38 -0400

    Reconfiguring index, compressing images, updating comps

--- dist/cic.js
+++ dist/cic.js
@@ -1,14 +1,7 @@
-var resizeViewPort = function(width, height) {
-    if (window.outerWidth) {
-        window.resizeTo(
-            width + (window.outerWidth - window.innerWidth),
-            height + (window.outerHeight - window.innerHeight)
-        );
-    } else {
-        window.resizeTo(1280, 800);
-        window.resizeTo(
-            width + (1280 - document.body.offsetWidth),
-            height + (800 - document.body.offsetHeight)
-        );
-    }
+window.onload = function(e){
+  window.resizeTo(1284, 800);
 };
+
+// var resizeViewPort = function(width, height) {
+//   window.resizeTo(1284, 800);
+// };
